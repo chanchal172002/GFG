9d83d69be82a69286497b08206ae5c60d93f3e37
Chanchal chauhan <81017907+chanchal172002@users.noreply.github.com>
2025-03-21 22:13:00 +0530

    Update word_ladder_1.cpp

--- graph/word_ladder_1.cpp
+++ graph/word_ladder_1.cpp
@@ -30,3 +30,35 @@
         }
     return 0;
     }
+
+
+//GETTING TLE IN LEETCODE
+
+int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string>s;
+        for(auto &str : wordList) s.insert(str);
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
+
+        while(!q.empty()){
+            string str = q.front().first;
+            int dist = q.front().second;
+            q.pop();
+            s.erase(str);
+            if(str==endWord)return dist;
+            for(int i=0;i<str.length();i++){
+                char original = str[i];
+                for(char j ='a'; j<='z';j++){
+                    if(j==original) continue;
+                    else{
+                        str[i]=j;
+                        if(s.find(str)!=s.end()){
+                            q.push({str,dist+1});
+                        }
+                    }
+                }
+                str[i]=original;
+            }
+        }
+        return 0;
+    }
